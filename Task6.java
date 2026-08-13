5cba56a4da4cc4562a01c3571eccc966b284b97e
S Naga Phanindra Reddy <vtu30153@veltech.edu.in>
2026-08-13 22:37:51 +0530

    Add files via upload

---
+++ Task6.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Task6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+        int evenSum = 0;
+        int oddSum = 0;
+        while (num > 0) {
+            int digit = num % 10;
+            if (digit % 2 == 0) {
+                evenSum = evenSum + digit;
+            } else {
+                oddSum = oddSum + digit;
+            }
+            num = num / 10; 
+        }
+        System.out.println("Sum of even digits =  " + evenSum);
+        System.out.println("Sum of odd digits =  " + oddSum);
+        sc.close();
+    }
+}
