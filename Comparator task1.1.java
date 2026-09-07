c8e20ad60e8f83f159ea31d4e64370ca0c29ad55
S Naga Phanindra Reddy <vtu30153@veltech.edu.in>
2026-09-07 19:24:30 +0530

    Add files via upload

---
+++ Comparator task1.1.java
@@ -0,0 +1,12 @@
+class Checker implements Comparator<Player> {
+    @Override
+    public int compare(Player a, Player b) {
+        // First compare scores in descending order
+        if (a.score != b.score) {
+            return Integer.compare(b.score, a.score);
+        }
+        
+        // If scores are equal, compare names in ascending alphabetical order
+        return a.name.compareTo(b.name);
+    }
+}
